c803d1561e1eee29179bdce10cfb30169d7acb2c
dbEsq <dougmberman@gmail.com>
2019-08-13 19:03:04 -0500

    Update for Heroku errors 1

---
+++ config/secret.js
@@ -0,0 +1,7 @@
+module.exports = {
+
+  database: 'mongodb://me:abcdef123@cluster0-shard-00-00-jt7lz.mongodb.net:27017,cluster0-shard-00-01-jt7lz.mongodb.net:27017,cluster0-shard-00-02-jt7lz.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority',
+  port: process.env.PORT || 3030,
+  secret: process.env.SECRET || 'fiverrclone2222',
+
+}
